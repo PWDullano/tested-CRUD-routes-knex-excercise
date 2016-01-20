f71aede7b1f5bad174615fca25736770dcba18e4
Patrick Dullano <PWDullano@gmail.com>
2016-01-19 19:07:58 -0700

    finished first set of tests

--- routes/movies.js
+++ routes/movies.js
@@ -2,32 +2,31 @@ var express = require('express');
 var router = express.Router();
 
 /* Movies Routes Go Here. */
-
-router.get('/', function(req, res, next){
+router.get('/', function(req,res,next){
   res.render('movies/index')
 })
 
-router.get('/new', function(req, res, next){
+router.get('/new', function(req,res,next){
   res.render('movies/new')
 })
 
-router.post('/', function(req, res, next){
+router.post('/', function(req,res,next){
   res.redirect('/movies')
 })
 
-router.get('/:id', function(req, res, next){
+router.get('/:id', function(req,res,next){
   res.render('movies/show')
 })
 
-router.get('/:id/edit', function(req, res, next){
+router.get('/:id/edit', function(req,res,next){
   res.render('movies/edit')
 })
 
-router.post('/:id', function(req, res, next){
+router.post('/:id', function(req,res,next){
   res.redirect('/movies')
 })
 
-router.post('/:id/delete', function(req, res, next){
+router.post('/:id/delete', function(req,res,next){
   res.redirect('/movies')
 })
 
