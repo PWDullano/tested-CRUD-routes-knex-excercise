396941399d8db0afd75953790c91994ce9e1ca70
Patrick Dullano <PWDullano@gmail.com>
2016-01-19 18:31:36 -0700

    first set of tests done

--- routes/movies.js
+++ routes/movies.js
@@ -3,32 +3,32 @@ var router = express.Router();
 
 /* Movies Routes Go Here. */
 
-router.get('/', function(req,res,next){
+router.get('/', function(req, res, next){
   res.render('movies/index')
 })
 
-router.get('/new', function(req,res,next){
+router.get('/new', function(req, res, next){
   res.render('movies/new')
 })
 
-router.post('/', function(req,res,next){
-  res.redirect('/')
+router.post('/', function(req, res, next){
+  res.redirect('/movies')
 })
 
-router.get('/:id', function(req,res,next){
+router.get('/:id', function(req, res, next){
   res.render('movies/show')
 })
 
-router.get('/:id/edit', function(req,res,next){
+router.get('/:id/edit', function(req, res, next){
   res.render('movies/edit')
 })
 
-router.post('/:id', function(req,res,next){
-  res.redirect('/')
+router.post('/:id', function(req, res, next){
+  res.redirect('/movies')
 })
 
-router.post('/:id/delete', function(req,res,next){
-  res.redirect('/')
+router.post('/:id/delete', function(req, res, next){
+  res.redirect('/movies')
 })
 
 module.exports = router;
